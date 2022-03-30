fcd8c255f3eb7e5221694a81e588f34aa4768b9d
Luke <lukejudges@gmail.com>
2022-03-30 08:49:13 +0100

    terminal additions

--- src/views/terminal/index.tsx
+++ src/views/terminal/index.tsx
@@ -11,5 +11,21 @@ export const TerminalView: FC = ({ }) => {
         SOLANA & NFT'S 101
       </h1>
       </div>
-  );
+      jQuery(function($, undefined) {
+          $('#term_demo').terminal(function(command) {
+              if (command !== '') {
+                  var result = window.eval(command);
+                  if (result != undefined) {
+                      this.echo(String(result));
+                  }
+              }
+          }, {
+              greetings: 'Javascript Interpreter',
+              name: 'js_demo',
+              height: 200,
+              width: 450,
+              prompt: 'js> '
+          });
+      });
+    );
 };
