e9f6d54c5d164e393154769258db26e04b47b8df
Luke <lukejudges@gmail.com>
2022-03-30 08:46:19 +0100

    terminal

--- src/views/terminal/index.tsx
+++ src/views/terminal/index.tsx
@@ -4,6 +4,9 @@ export const TerminalView: FC = ({ }) => {
 
   return (
       <div>
+      <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.terminal/2.32.1/js/jquery.terminal.min.js"></script>
+      <link href="https://cdnjs.cloudflare.com/ajax/libs/jquery.terminal/2.32.1/css/jquery.terminal.min.css" rel="stylesheet"/>
       <h1 className="text-center text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-tr from-[#9945FF] to-[#14F195]">
         SOLANA & NFT'S 101
       </h1>
