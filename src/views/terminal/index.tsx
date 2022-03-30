0999ce7575c2943346def52d06750900d4b23013
Luke <lukejudges@gmail.com>
2022-03-30 09:21:53 +0100

    terminal jquery application

--- src/views/terminal/index.tsx
+++ src/views/terminal/index.tsx
@@ -1,30 +1,29 @@
 import { FC } from "react";
-import $ from "jquery";
+import * as $ from "jquery";
 
 export const TerminalView: FC = ({ }) => {
 
   return (
-      <div className="application">
+      jQuery(function($, undefined) {
+          $('#term_demo').terminal(function(command) {
+              if (command !== '') {
+                  var result = window.eval(command);
+                  if (result != undefined) {
+                      this.echo(String(result));
+                  }
+              }
+          }, {
+              greetings: 'Javascript Interpreter',
+              name: 'js_demo',
+              height: 200,
+              width: 450,
+              prompt: 'js> '
+          });
+      });
+      <div className="Jquery application">
       <script src="jquery-3.6.0.min.js"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.terminal/2.32.1/js/jquery.terminal.min.js"></script>
       <link href="https://cdnjs.cloudflare.com/ajax/libs/jquery.terminal/2.32.1/css/jquery.terminal.min.css" rel="stylesheet"/>
       </div>
   );
 };
-
-jQuery(function($, undefined) {
-    $('#term_demo').terminal(function(command) {
-        if (command !== '') {
-            var result = window.eval(command);
-            if (result != undefined) {
-                this.echo(String(result));
-            }
-        }
-    }, {
-        greetings: 'Javascript Interpreter',
-        name: 'js_demo',
-        height: 200,
-        width: 450,
-        prompt: 'js> '
-    });
-});
