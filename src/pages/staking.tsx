b4657b7df8e467f2068849781b3e51592961fba9
Luke <lukejudges@gmail.com>
2022-03-30 13:01:29 +0100

    staking links

--- src/pages/staking.tsx
+++ src/pages/staking.tsx
@@ -10,7 +10,7 @@ const Staking: NextPage = (props) => {
         <meta/>
       </Head>
       <h1 className="text-center text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-tr from-[#9945FF] to-[#14F195]"> COMING SOON... </h1>
-      <Stakingiew />
+      <StakingView />
     </div>
   );
 };
