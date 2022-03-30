db2e96c7bf018602efe65031dcd3ce837a30ad8a
Luke Judges Remote <lukejudges@hotmail.com>
2022-03-30 18:14:33 +0100

    teams edit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -188,7 +188,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4553967fd5ba4333"; }
+/******/ 		__webpack_require__.h = function() { return "8bef1d79b9b49d76"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/views/team/index.tsx
+++ src/views/team/index.tsx
@@ -56,13 +56,6 @@ export const TeamView: FC = ({ }) => {
                                     <div className="w-full flex justify-center pt-5 pb-5">
                                         <a href="javascript:void(0)" className="mx-5">
                                             <div>
-                                                <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="#718096" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" className="feather feather-github">
-                                                    <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22" />
-                                                </svg>
-                                            </div>
-                                        </a>
-                                        <a href="javascript:void(0)" className="mx-5">
-                                            <div>
                                                 <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="#718096" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" className="feather feather-twitter">
                                                     <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z" />
                                                 </svg>
@@ -80,17 +73,10 @@ export const TeamView: FC = ({ }) => {
                                     </div>
                                 </div>
                                 <div className="px-6 mt-16">
-                                    <div className="font-bold text-3xl text-white text-center pb-1">El Davo</div>
-                                    <p className="text-white text-sm text-center">Artist</p>
-                                    <p className="text-center text-gray-400 text-base pt-3 font-normal">El Davo is a UK based freelance illustrator, painter and mural artist who doesn’t want to be solely defined by these labels as he believes he can do anything.</p>
+                                    <div className="font-bold text-3xl text-white text-center pb-1">Callum</div>
+                                    <p className="text-white text-sm text-center">Economy & Tokenomics</p>
+                                    <p className="text-center text-gray-400 text-base pt-3 font-normal">Callum is a PHD Economics student at UCL and is repsonsible for designing the economic model for our token. </p>
                                     <div className="w-full flex justify-center pt-5 pb-5">
-                                        <a href="javascript:void(0)" className="mx-5">
-                                            <div>
-                                                <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="#718096" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" className="feather feather-github">
-                                                    <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22" />
-                                                </svg>
-                                            </div>
-                                        </a>
                                         <a href="javascript:void(0)" className="mx-5">
                                             <div>
                                                 <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="#718096" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" className="feather feather-twitter">
