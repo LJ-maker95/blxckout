6dbd6b63d54b4c8c31f05b795407b6aa397ffb6d
Luke <lukejudges@gmail.com>
2022-03-30 08:59:18 +0100

    terminal app

--- src/views/terminal/index.tsx
+++ src/views/terminal/index.tsx
@@ -3,10 +3,7 @@ import { FC } from "react";
 export const TerminalView: FC = ({ }) => {
 
   return (
-      <div>
-      <h1 className="text-center text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-tr from-[#9945FF] to-[#14F195]">
-        SOLANA & NFT'S 101
-      </h1>
+      <div className="application">
       <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.terminal/2.32.1/js/jquery.terminal.min.js"></script>
       <link href="https://cdnjs.cloudflare.com/ajax/libs/jquery.terminal/2.32.1/css/jquery.terminal.min.css" rel="stylesheet"/>
