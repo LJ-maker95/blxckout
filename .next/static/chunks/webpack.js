13b402827051775b317c10f30f05c5a8a4101aee
Luke Judges Remote <lukejudges@hotmail.com>
2022-04-09 14:05:49 +0100

    art page added

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -188,7 +188,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8386dfcf49bc43f4"; }
+/******/ 		__webpack_require__.h = function() { return "ed96889036cfcd7f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
