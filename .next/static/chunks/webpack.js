2cdf37c9d9bbc05ce52f65969b2670002c042b77
Luke Judges Remote <lukejudges@hotmail.com>
2022-04-09 14:12:43 +0100

    art page added

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -188,7 +188,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f31e632fc5baacc9"; }
+/******/ 		__webpack_require__.h = function() { return "dc5a26ed988fb976"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/views/basics/index.tsx
+++ src/views/basics/index.tsx
@@ -41,7 +41,7 @@ export const BasicsView: FC = ({ }) => {
             <div className="flex flex-wrap my-12 dark:text-white">
                 <div className="w-full border-b md:w-1/2 md:border-r lg:w-1/3 p-8">
                     <div className="flex items-center mb-6">
-                        <svg width="20" height="20" fill="currentColor" class="h-6 w-6 text-indigo-500" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                        <svg width="20" height="20" fill="currentColor" className="h-6 w-6 text-indigo-500" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
                             <path d="M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z">
                             </path>
                         </svg>
@@ -55,7 +55,7 @@ export const BasicsView: FC = ({ }) => {
                 </div>
                 <div className="w-full border-b md:w-1/2 lg:w-1/3 lg:border-r p-8">
                     <div className="flex items-center mb-6">
-                        <svg width="20" height="20" fill="currentColor" class="h-6 w-6 text-indigo-500" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                        <svg width="20" height="20" fill="currentColor" className="h-6 w-6 text-indigo-500" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
                             <path d="M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z">
                             </path>
                         </svg>
@@ -69,7 +69,7 @@ export const BasicsView: FC = ({ }) => {
                 </div>
                 <div className="w-full border-b md:w-1/2 md:border-r lg:w-1/3 lg:border-r-0 p-8">
                     <div className="flex items-center mb-6">
-                        <svg width="20" height="20" fill="currentColor" class="h-6 w-6 text-indigo-500" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                        <svg width="20" height="20" fill="currentColor" className="h-6 w-6 text-indigo-500" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
                             <path d="M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z">
                             </path>
                         </svg>
@@ -83,7 +83,7 @@ export const BasicsView: FC = ({ }) => {
                 </div>
                 <div className="w-full border-b md:w-1/2 lg:w-1/3 lg:border-r lg:border-b-0 p-8">
                     <div className="flex items-center mb-6">
-                        <svg width="20" height="20" fill="currentColor" class="h-6 w-6 text-indigo-500" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                        <svg width="20" height="20" fill="currentColor" className="h-6 w-6 text-indigo-500" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
                             <path d="M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z">
                             </path>
                         </svg>
@@ -97,7 +97,7 @@ export const BasicsView: FC = ({ }) => {
                 </div>
                 <div className="w-full border-b md:w-1/2 md:border-r md:border-b-0 lg:w-1/3 lg:border-b-0 p-8">
                     <div className="flex items-center mb-6">
-                        <svg width="20" height="20" fill="currentColor" class="h-6 w-6 text-indigo-500" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                        <svg width="20" height="20" fill="currentColor" className="h-6 w-6 text-indigo-500" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
                             <path d="M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z">
                             </path>
                         </svg>
@@ -111,7 +111,7 @@ export const BasicsView: FC = ({ }) => {
                 </div>
                 <div className="w-full md:w-1/2 lg:w-1/3 p-8">
                     <div className="flex items-center mb-6">
-                        <svg width="20" height="20" fill="currentColor" class="h-6 w-6 text-indigo-500" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                        <svg width="20" height="20" fill="currentColor" className="h-6 w-6 text-indigo-500" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
                             <path d="M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z">
                             </path>
                         </svg>
