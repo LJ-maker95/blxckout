5f341976d22b8f39d9077ab10600ba5a8a21931f
LJ <lukejudges@LJs-MacBook-Pro.local>
2022-06-11 14:32:38 +0100

    page changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,5 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { HomeView } from "../views";
-import Router from 'next/router';
 
 const Home: NextPage = (props) => {
   return (
